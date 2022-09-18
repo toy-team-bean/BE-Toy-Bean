ca70d98ef430446f641ff4af1650ea73b1d74121
sungbeen <sbkang@datarse.co.kr>
2022-09-18 16:49:05 +0900

    dfdsfsd

---
+++ src/test/java/beanProject/toybeanproject/ToyBeanProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package beanProject.toybeanproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyBeanProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
