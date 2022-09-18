db8ef0aeeec0f63a7450d910a4f9c2ed82331f81
sungbeen <sbkang@datarse.co.kr>
2022-09-18 16:06:27 +0900

    Initial commit

---
+++ src/main/java/beanProject/toybeanproject/ToyBeanProjectApplication.java
@@ -0,0 +1,13 @@
+package beanProject.toybeanproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyBeanProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyBeanProjectApplication.class, args);
+	}
+
+}
